93d12e1a752fc25c6cbcd9e2295391dffd83b8f0
Michael Fahim <michael_fahim@yahoo.com>
2020-04-24 00:50:57 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,13 @@
 class Song < ActiveRecord::Base
+  belongs_to :artist
+  belongs_to :genre
 
+  def get_genre_name
+    self.genre.name
+  end
+
+  def drake_made_this
+    drake = Artist.find_or_create_by(name: "Drake")
+    self.artist = drake
+  end
 end
